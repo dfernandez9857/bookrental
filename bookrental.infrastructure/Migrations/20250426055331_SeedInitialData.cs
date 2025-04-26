583821e0e0d48db288002c0920fb883ea9b3762d
Diego Fernández <dfernandez9857@gmail.com>
2025-04-26 01:07:59 -0500

    INC-007 :  add SeedData Migrations

---
+++ bookrental.infrastructure/Migrations/20250426055331_SeedInitialData.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bookrental.infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder
+              .Sql("INSERT INTO cliente(nombres, apellidos, documento_identidad) VALUES('Franco Diego', 'Fernandez Cruz', '48358399');");
+            migrationBuilder
+               .Sql("INSERT INTO cliente(nombres, apellidos, documento_identidad) VALUES('Manuel', 'Neuer', '234345');");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ bookrental.infrastructure/Migrations/20250426060217_SeedBookCopy.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bookrental.infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedBookCopy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder
+             .Sql("INSERT INTO ejemplar(habilitado_prestamo, estado_ejemplar_id) VALUES(1, 1);");
+            migrationBuilder
+              .Sql("INSERT INTO ejemplar(habilitado_prestamo, estado_ejemplar_id) VALUES(1, 1);");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
